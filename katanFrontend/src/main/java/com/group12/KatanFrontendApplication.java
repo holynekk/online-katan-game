f0c3e3d027c36c86319ffa7bc14ba8aa11cd587d
holynekk <e238109@metu.edu.tr>
2023-12-28 16:26:26 +0300

    Initial project files for katanFrontend.

---
+++ katanFrontend/src/main/java/com/group12/KatanFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.group12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatanFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KatanFrontendApplication.class, args);
+	}
+
+}
---
+++ katanFrontend/src/test/java/com/group12/KatanFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KatanFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
